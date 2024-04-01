8e58d06c8cc683479050ef778f30730d97011683
Guilherme <bittenkourt.gmf@gmail.com>
2024-04-01 20:30:14 -0300

    Input fields implemented

---
+++ constants/Constants.py
@@ -0,0 +1,19 @@
+EXERCICES = {
+    "Peito": ["Flexão", "Supino Reto", "Supino Inclinado", "Crossover", "Voador", "Supino Reto Articulado"],
+    "Costas": ["Puxador Frontal", "Remada Curvada", "Remada Baixa", "Barra Fixa", "Pulley Articulado", "Remada Cavalinho", "Pullover", "Fly Inverso"],
+    "Tríceps": ["Polia", "Corda", "Mergulho", "Frances", "Coice"],
+    "Bíceps": ["Martelo", "Rosca Direta", "Scott"],
+    "Ombro": ["Elevação Lateral", "Remada Alta", "Fly Inverso", "Desenvolvimento Livre", "Desenvolvimento com Halter"],
+    "Abdominal": ["Infra", "Militar", "Prancha"],
+    "Quadríceps": ["Agachamento Livre", "Mesa Extensora", "Agachamento Isométrico"],
+    "Posterior": ["Mesa Flexora", "Flexão em Pé", "Cadeira Flexora"],
+    "Glúteo": ["Abdutor", "Adutor", "Elevação Pélvica"],
+    "Trapézio": ["Elevação"],
+    "Antebraço": ["Rosca Inversa", "Rosca Parafuso"],
+    "Lombar": ["Cadeira"],
+    "Panturrilha": ["Cadeira"]
+}
+
+SERIES = [3,4,5]   
+
+REPETITIONS = [12, 15, 20, 24]
--- pages/Gym.py
+++ pages/Gym.py
@@ -1,13 +1,34 @@
 from library import *
 from services.Database import Database
+from constants.Constants import *
 
 st.set_page_config(layout="wide")
 
-st.title("📁 Crypto Trades Registed")
-st.markdown("You can see all your trades bellow!")
+st.title("📁 Gym Dashboard")
+st.markdown("You can see all information bellow!")
 
 database = Database(worksheets=[
     ("GYM", 6)
 ])
 
-st.dataframe(database.worksheets["GYM"])
+# st.dataframe(database.worksheets["GYM"])
+
+tab1, tab2 = st.tabs(["Analysis", "Register Fit"])
+
+with tab1:
+    ...
+
+with tab2:
+    date = st.date_input(label="Today")
+
+    muscle_group = st.selectbox(label="Select Group Muscle", options=EXERCICES.keys())
+
+    exercice = None
+    if muscle_group != None:
+        exercice = st.selectbox(label="Choice some exercise", options=EXERCICES[muscle_group])
+
+    weight = st.number_input(label="Weight Value", step=1)
+    
+    series = st.selectbox(label="Choice a serie", options=SERIES)
+
+    repetitions = st.selectbox(label="Choice a repetition size", options=REPETITIONS)
